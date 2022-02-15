9d9632036d6146a2b64986fe27c2313125dc8f00
HaneenFarhat <haneenfarhat.33@gmail.com>
2022-02-15 17:04:34 +0200

    aaaaa

---
+++ function/main.js
@@ -0,0 +1,210 @@
+
+
+
+// const findKey =function(str){
+
+//     let key =''
+// let isReagingKey=false
+
+//   for (let i = 0;i < str.length;  i++) {
+//   if (str[i]=='#') { 
+
+//     isReagingKey=!isReagingKey
+
+
+//  }
+
+//  if (isReagingKey && str[i]!='#')
+
+// key +=str[i] 
+// }
+
+
+//     return key
+// }
+
+
+
+// const str ='gfyfylfuy #cat# y gyfyoulf gpu'
+
+// console.log(
+//     findKey(str)
+// );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const replaceKey=function(str,value){
+
+//     let newStr = ''
+// let isReagingKey= false
+// let foundKey = false
+
+//   for (let i = 0;str[i];  i++) {
+//   if (str[i]=='#') { 
+
+//     isReagingKey=!isReagingKey
+//     foundKey = false
+
+//  }
+
+//  if (isReagingKey){
+// if (!founKey){
+// newStr +=value
+// foundKey = true
+
+// }
+
+
+
+//  }else{
+
+//     newStr +=str[i] || ''
+
+
+
+
+//   }
+
+
+
+// }
+
+
+// console.log(newStr);
+// return newStr
+
+
+// }
+
+
+
+
+
+
+
+// new thing
+
+
+
+
+
+
+
+
+const arr1 = ['haneen', 'ahmad', 'adam', 'yazeed', 'rami']
+
+
+const sayHello = function (arr) {
+
+
+
+    for (let i = 0; i < arr.length; i++) {
+
+
+        console.log("Hello " + arr[i]);
+    }
+}
+sayHello(arr1)
+
+
+
+
+
+let num1 =5
+let num2 = num1
+
+num2++
+//   ps zaad 1
+
+
+// num+=3 
+// more then one
+
+
+
+
+
+
+
+
+const formatArr= function (arr) {
+
+
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = "hello " + arr[i]
+
+    }
+}
+formatArr(arr1)
+console.log(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
